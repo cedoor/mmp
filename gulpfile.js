388e246abcee0908b9947c6db778e7cf6d47faa3
cedoor <omardesogus9@gmail.com>
2016-11-03 11:35:47 +0100

    build: support to es6 syntax with uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,43 @@
 var gulp = require('gulp'),
     del = require('del'),
+    pump = require('pump'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
+    uglify = require('uglify-js'),
+    minifier = require('gulp-uglify/minifier'),
     rename = require('gulp-rename');
 
 gulp.task('default', ['watch'], function() {
     gulp.start('compileSrc','compileTest');
 });
 
-gulp.task('compileSrc', function() {
-    return gulp.src([
-        'src/start.js',
-        'src/init.js',
-        'src/utils.js',
-        'src/public.js',
-        'src/end.js'
-    ])
-    .pipe(concat('mmap.js'))
-    .pipe(gulp.dest('dist'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+gulp.task('compileSrc', function(cb) {
+    pump([
+        gulp.src([
+            'src/start.js',
+            'src/init.js',
+            'src/utils.js',
+            'src/public.js',
+            'src/end.js'
+        ]),
+        concat('mmap.js'),
+        gulp.dest('dist'),
+        rename({suffix: '.min'}),
+        minifier({}, uglify),
+        gulp.dest('dist')
+    ], cb );
 });
 
-gulp.task('compileTest', function() {
-    return gulp.src([
-        'test/unit/start.js',
-        'test/unit/init.js',
-        'test/unit/nodes.js',
-        'test/unit/end.js'
-    ])
-    .pipe(concat('test.js'))
-    .pipe(gulp.dest('test'));
+gulp.task('compileTest', function(cb) {
+    pump([
+        gulp.src([
+            'test/unit/start.js',
+            'test/unit/init.js',
+            'test/unit/nodes.js',
+            'test/unit/end.js'
+        ]),
+        concat('test.js'),
+        gulp.dest('test')
+    ], cb );
 });
 
 gulp.task('watch', function() {
