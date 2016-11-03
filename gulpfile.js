a80e5d3a31088cd272ffb7843960cf8dd32ef1ca
cedoor <omardesogus9@gmail.com>
2016-11-03 11:54:04 +0100

    build: remove unused node packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp'),
-    del = require('del'),
-    pump = require('pump'),
     concat = require('gulp-concat'),
     uglify = require('uglify-js'),
     minifier = require('gulp-uglify/minifier'),
@@ -11,33 +9,29 @@ gulp.task('default', ['watch'], function() {
 });
 
 gulp.task('compileSrc', function(cb) {
-    pump([
-        gulp.src([
-            'src/start.js',
-            'src/init.js',
-            'src/utils.js',
-            'src/public.js',
-            'src/end.js'
-        ]),
-        concat('mmap.js'),
-        gulp.dest('dist'),
-        rename({suffix: '.min'}),
-        minifier({}, uglify),
-        gulp.dest('dist')
-    ], cb );
+    return gulp.src([
+        'src/start.js',
+        'src/init.js',
+        'src/utils.js',
+        'src/public.js',
+        'src/end.js'
+    ])
+    .pipe(concat('mmap.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifier({}, uglify))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('compileTest', function(cb) {
-    pump([
-        gulp.src([
-            'test/unit/start.js',
-            'test/unit/init.js',
-            'test/unit/nodes.js',
-            'test/unit/end.js'
-        ]),
-        concat('test.js'),
-        gulp.dest('test')
-    ], cb );
+    return gulp.src([
+        'test/unit/start.js',
+        'test/unit/init.js',
+        'test/unit/nodes.js',
+        'test/unit/end.js'
+    ])
+    .pipe(concat('test.js'))
+    .pipe(gulp.dest('test'));
 });
 
 gulp.task('watch', function() {
