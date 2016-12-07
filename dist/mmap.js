ab424c915fbbd809c64fd673b041e0a7b9b2564a
cedoor <omardesogus9@gmail.com>
2016-12-07 12:17:12 +0100

    fix conflicts

--- dist/mmap.js
+++ dist/mmap.js
@@ -220,33 +220,6 @@
         }
     }
 
-    /****** Link functions  ******/
-
-    function drawLink( n ) {
-
-        const width = 30 - getNodeLevel( n ) * 5;
-        const orY = n.parent.y < n.y ? -1 : 1;
-        const orX = n.parent.x > n.x ? -1 : 1;
-        const middleX = ( n.parent.x + n.x ) / 2;
-        const k = n.k = n.k || d3.randomUniform(50)();
-
-        const path = d3.path();
-        path.moveTo( n.parent.x, n.parent.y - width/2 );
-        path.bezierCurveTo(
-            middleX, n.parent.y - width/2,
-            n.parent.x + k*orX, n.y + n.height/2 + 5 + k/10,
-            n.x - ( n.width/4 - k/2 )*orX, n.y + n.height/2 + 5 + k/10
-        );
-        path.bezierCurveTo(
-            n.parent.x + k*orX + width/2*orY*orX, n.y + n.height/2 + width/2 + 5 + k/10,
-            middleX + width/2*orY*orX, n.parent.y + width/2,
-            n.parent.x, n.parent.y + width/2
-        );
-        path.closePath();
-
-        return path;
-    }
-
     /****** Public functions ******/
 
     const events = d3.dispatch(
