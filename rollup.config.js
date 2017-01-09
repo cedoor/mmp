a64bc2b4caa204ee59594df00ce020f27b2da6d2
cedoor <omardesogus9@gmail.com>
2017-01-09 16:41:25 +0100

    (build) create new npm scripts and install test tool mocha

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,26 @@
 // Rollup plugins
-import babel from 'rollup-plugin-babel'
-import eslint from 'rollup-plugin-eslint'
-import uglify from 'rollup-plugin-uglify'
-import progress from 'rollup-plugin-progress'
 import json from 'rollup-plugin-json'
-import fs from 'fs'
+import babel from 'rollup-plugin-babel'
 
-const
-    env = process.env.NODE_ENV === 'production',
-    pkg = JSON.parse( fs.readFileSync('package.json', 'utf8') ),
-    dest = 'dist/' + pkg.name + ( env ? '.min.js' : '.js' ),
-    banner = `/**
- * ${pkg.name} v${pkg.version} ${pkg.homepage}
- * Copyright ${new Date().getFullYear()} ${pkg.author.name}
- * Lincensed under ${pkg.license}
- */`
+const pkg = require('./package.json')
 
 export default {
-    banner: banner,
     entry: 'index.js',
-    dest: dest,
+    dest: 'build/'+ pkg.name +'.js',
     format: 'umd',
     moduleName: pkg.name,
     external: ['d3'],
     globals: { d3: 'd3' },
     //sourceMap: 'inline',
     plugins: [
-        json({ exclude: 'node_modules/**' }),
-        progress(),
-        eslint(),
+        json(),
         babel({
-            exclude: 'node_modules/**',
             presets: ["es2015-rollup"]
-        }),
-        ( env && uglify() )
-    ]
+        })
+    ],
+    banner : `/**
+ * ${pkg.name} v${pkg.version} ${pkg.homepage}
+ * Copyright ${new Date().getFullYear()} ${pkg.author.name}
+ * Lincensed under ${pkg.license}
+ */`
 }
---
+++ test/init.js
@@ -0,0 +1,12 @@
+const
+    mmap = require('../build/mmap'),
+    assert = require('assert')
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1,2,3].indexOf(4))
+            assert.equal(mmap.version, '0.1.3')
+        })
+    })
+})
