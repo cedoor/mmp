d4e55df72aac444fd855dd8e298ae3c2ba4c17d2
cedoor <omardesogus9@gmail.com>
2017-01-15 20:21:09 +0100

    trivial updates

--- test/tests.js
+++ test/tests.js
@@ -2,33 +2,17 @@ describe('Tests', function() {
 
     var assert = chai.assert;
 
-    describe('#version', function() {
+    describe('#mmap.version()', function() {
         it('Current version of mmap', function() {
             assert.equal( mmap.version, '0.1.3' );
         });
     });
 
-    describe('#mmap.init()', function() {
+    describe('#mmap.select()', function() {
         it('should initialize the mind map', function() {
             var selected = mmap.node.select();
             assert.equal( selected.value.name, 'Root node');
         });
     });
 
-    describe('#mmap.node.add()', function() {
-        it('should initialize the mind map', function() {
-            mmap.node.add({'name': 'Child node'});
-            mmap.node.select('node1');
-            var selected = mmap.node.select();
-            assert.equal( selected.key, 'node1' );
-        });
-    });
-
-    describe('#mmap.node.remove()', function() {
-        it('should initialize the mind map', function() {
-            mmap.node.remove();
-            var selected = mmap.node.select();
-            assert.equal( selected.key, 'node0' );
-        });
-    });
 })
